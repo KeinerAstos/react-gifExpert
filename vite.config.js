7b46f73b7d300c09607e6459f49e7e0110389c1f
keiner <keiner3462@gmail.com>
2025-08-17 11:27:45 -0500

    Corrige base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vue-app/',
+  base: '/react-gifExpert/',   // 👈 debe coincidir exactamente con el nombre del repo
 })
